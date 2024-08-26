6046b9b119dbaf6ba3672ccb7738361b383c67ca
Feel <thinkhero@gmail.com>
2024-08-26 21:50:19 +0900

    xml applicationContext.xml

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,5 +1,6 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
 import springbook.user.domain.User;
 
 import java.sql.Connection;
@@ -7,11 +8,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 public class UserDao {
 
     private ConnectionMaker connectionMaker;
-    private Connection c;
-    private User user;
+
 
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
@@ -19,38 +20,37 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         // JDBC API를 이용한 사용자 등록 코드
-        this.c = connectionMaker.makeConnection();
-        this.user = user;
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
 
-        ps.setString(1, this.user.getId());
-        ps.setString(2, this.user.getName());
-        ps.setString(3, this.user.getPassword());
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
 
         ps.executeUpdate();
 
         ps.addBatch();
-        this.c.close();
+        c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
         // JDBC API를 이용한 사용자 조회 코드
-        this.c = connectionMaker.makeConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
         rs.next();
 
-
-        this.user.setId(rs.getString("id"));
-        this.user.setName(rs.getString("name"));
-        this.user.setPassword(rs.getString("password"));
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
 
         rs.close();
         ps.addBatch();
         c.close();
 
-        return this.user;
+        return user;
     }
 }
--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,6 +2,7 @@ package springbook.user.dao;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -10,7 +11,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
