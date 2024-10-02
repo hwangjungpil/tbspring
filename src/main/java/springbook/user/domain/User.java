521197970821dacd5b95820a28b3270a3d395418
think <thinkhero@gmail.com>
2024-10-02 21:27:14 +0900

    173p

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -1,10 +1,16 @@
 package springbook.user.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class User {
     String id;
     String name;
     String password;
+
+    public User() {
+    }
 }
+
