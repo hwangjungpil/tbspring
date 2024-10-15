c3b04c8515410431d7695050e017fe6b3d0d88f6
think <thinkhero@gmail.com>
2024-10-15 21:58:45 +0900

    237p

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -15,6 +15,12 @@ public class UserDao {
 
     private DataSource dataSource;
 
+    private JdbcContext jdbcContext;
+
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
+
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -51,24 +57,30 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+       this.jdbcContext.workWithStatementStrategy(
+               new StatementStrategy() {
+                   @Override
+                   public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                       return c.prepareStatement("delete from users");
+                   }
+               }
+       );
     }
 
     public void add(final User user) throws ClassNotFoundException, SQLException {
-        jdbcContextWithStatementStrategy(
-                    new StatementStrategy() {
-                        @Override
-                        public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                            PreparedStatement preparedStatement = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+        this.jdbcContext.workWithStatementStrategy(
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
 
-                            preparedStatement.setString(1, user.getId());
-                            preparedStatement.setString(2, user.getName());
-                            preparedStatement.setString(3, user.getPassword());
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
 
-                            return preparedStatement;
-                        }
+                        return ps;
                     }
+                }
         );
 
     }
